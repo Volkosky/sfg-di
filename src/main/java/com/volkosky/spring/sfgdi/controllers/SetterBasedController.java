863bfbb706d4296caa59fc0b91b01901424577df
Mateusz Wołek <volkosky@gmail.com>
2020-11-03 23:01:15 +0100

    Spring DI

--- src/main/java/com/volkosky/spring/sfgdi/controllers/SetterBasedController.java
+++ src/main/java/com/volkosky/spring/sfgdi/controllers/SetterBasedController.java
@@ -1,11 +1,15 @@
 package com.volkosky.spring.sfgdi.controllers;
 
 import com.volkosky.spring.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterBasedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
