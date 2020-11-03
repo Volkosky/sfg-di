804eb7dcb3a503e0ec36bcd39db4275b119cbdb4
Mateusz Wołek <volkosky@gmail.com>
2020-11-03 23:15:13 +0100

    Spring DI - primary

--- src/main/java/com/volkosky/spring/sfgdi/controllers/MyController.java
+++ src/main/java/com/volkosky/spring/sfgdi/controllers/MyController.java
@@ -1,12 +1,18 @@
 package com.volkosky.spring.sfgdi.controllers;
 
+import com.volkosky.spring.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello!");
-        return "Hi everybody!";
+        return greetingService.sayGreeting();
     }
 }
